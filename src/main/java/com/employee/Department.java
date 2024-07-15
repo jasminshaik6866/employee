1b984b2eb3ca8989ecc97b12f749442fde4ae6c9
jasminshaik6866 <jasminshaik6866@gmail.com>
2024-07-15 22:33:54 +0530

    create department  entity and its services

---
+++ src/main/java/com/employee/Department.java
@@ -0,0 +1,61 @@
+package com.employee;
+
+
+import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer departmentId;
+
+    private String name;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    // Getters and setters
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+
---
+++ src/main/java/com/employee/DepartmentServiceImp.java
@@ -0,0 +1,8 @@
+package com.employee;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImp implements DepartmentService {
+
+}
